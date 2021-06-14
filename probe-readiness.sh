51086260189ff040449c483048cf5b7484eb7e5b
Brad Smith <brasmith@redhat.com>
2021-06-14 16:17:44 -0400

    make probe-readiness.sh use variables for DB_USER and DB_NAME

--- probe-readiness.sh
+++ probe-readiness.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-PGPASSWORD="$DB_PASSWORD" /usr/pgsql-13/bin/psql -h localhost -p 5432 -U postgres -c "select 1 as psql_is_ready" 2>/dev/null | grep "psql_is_ready"
+# Note: host and port should remain localhost and 5432 because
+# those represent the location of pgbouncer itself, not the
+# remote psql server that would have $DB_HOST:$DB_PORT.
+
+PGPASSWORD="$DB_PASSWORD" /usr/pgsql-13/bin/psql \
+  -h localhost -p 5432 -U "${DB_USER}" -d "${DB_NAME}" \
+  -c "select 1 as psql_is_ready" 2>/dev/null \
+  | grep "psql_is_ready"
 exit $?
 
