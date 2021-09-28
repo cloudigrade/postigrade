21da1294875ada18045c08b8937509cc9d6b9656
Alberto Bellotti <abellott@redhat.com>
2021-09-28 11:43:43 -0400

    Adding support for SSL to the RDS Database with the ssl mode and RDS CA provided to us by clowder.

--- clowder_init.sh
+++ clowder_init.sh
@@ -8,7 +8,16 @@ else
   export DB_PORT="`cat $ACG_CONFIG | jq -r '.database.port // empty'`"
   export DB_USER="`cat $ACG_CONFIG | jq -r '.database.username // empty'`"
   export DB_PASSWORD="`cat $ACG_CONFIG | jq -r '.database.password // empty'`"
+  export DB_SSLMODE="`cat $ACG_CONFIG | jq -r '.database.sslMode // empty'`"
+  export DB_CAFILE="/etc/pgbouncer/rdsca.cert"
   export PG_BOUNCER_LISTEN_PORT="`cat $ACG_CONFIG | jq -r '.webPort'`"
+
+  db_cert="`cat $ACG_CONFIG | jq -r '.database.rdsCa // empty'`"
+  >${DB_CAFILE}
+  if [[ -n "${db_cert}" ]]; then
+    echo "${db_cert}" > ${DB_CAFILE}
+    unset db_cert
+  fi
 
   [[ -z "${DB_PORT}" ]] && DB_PORT="5432"
 fi
