0ee4ffa2c706577352b54b12e8f5aea0942728c9
Brad Smith <brasmith@redhat.com>
2022-06-22 15:41:52 -0400

    experimental debug output for debugging performance issues

--- probe-liveness.sh
+++ probe-liveness.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+PID="$$"
+LOGPATH=/tmp/log-liveness-"$PID"
+
+function logtime() {
+  echo "$(date --iso-8601=ns) $1" | tee -a $LOGPATH
+}
+
+
+exec {BASH_XTRACEFD}>>$LOGPATH
+#set -x
+
+
+logtime "before clowder init"
 source /clowder_init.sh
 
-/usr/pgsql-14/bin/pg_isready -h localhost -p ${PG_BOUNCER_LISTEN_PORT} -U "${DB_USER}" | grep "accepting connections"
-exit $?
+logtime "before pg_isready"
+/usr/pgsql-14/bin/pg_isready -h localhost -p ${PG_BOUNCER_LISTEN_PORT} -U "${DB_USER}" | grep "accepting connections"  | tee -a $LOGPATH
+RESULT=$?
+logtime "after pg_isready"
+echo "result was $RESULT" | tee -a $LOGPATH
+exit $RESULT
 
--- probe-readiness.sh
+++ probe-readiness.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+function logtime() {
+  echo "$(date --iso-8601=ns) $1" | tee -a $LOGPATH
+}
+
+
+PID="$$"
+LOGPATH=/tmp/log-readiness-"$PID"
+
+exec {BASH_XTRACEFD}>>$LOGPATH
+#set -x
+
 # Note: host and port should remain localhost and ${PG_BOUNCER_LISTEN_PORT}
 # because that represents the location of pgbouncer itself, not the
 # remote psql server that would have $DB_HOST:$DB_PORT.
-
 source /clowder_init.sh
 
+logtime "before psql select"
 PGPASSWORD="$DB_PASSWORD" /usr/pgsql-14/bin/psql \
   -h localhost -p ${PG_BOUNCER_LISTEN_PORT} -U "${DB_USER}" -d "${DB_NAME}" \
   -c "select 1 as psql_is_ready" 2>/dev/null \
-  | grep "psql_is_ready"
-exit $?
+  | grep "psql_is_ready" | tee -a $LOGPATH
+RESULT=$?
+
+logtime "psql finished"
+echo "result was $RESULT" | tee -a $LOGPATH
+exit $RESULT
 
