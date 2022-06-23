b48c1ed33ccfe2b77bf18d313d91d36364bb5c2a
Brad Smith <brasmith@redhat.com>
2022-06-23 12:57:43 -0400

    replace jq calls with json_to_env.py in clowder_init.sh because jq 1.6 is abysmally slow (10x or worse) than jq 1.5 and is causing probes to exceed timeouts

--- clowder_init.sh
+++ clowder_init.sh
@@ -1,18 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+env_file="/tmp/clowder_envs.sh"
 
 if [[ -z "${ACG_CONFIG}" ]]; then
   export PG_BOUNCER_LISTEN_PORT="5432"
-else
-  export DB_NAME="`cat $ACG_CONFIG | jq -r '.database.name // empty'`"
-  export DB_HOST="`cat $ACG_CONFIG | jq -r '.database.hostname // empty'`"
-  export DB_PORT="`cat $ACG_CONFIG | jq -r '.database.port // empty'`"
-  export DB_USER="`cat $ACG_CONFIG | jq -r '.database.username // empty'`"
-  export DB_PASSWORD="`cat $ACG_CONFIG | jq -r '.database.password // empty'`"
-  export DB_SSLMODE="`cat $ACG_CONFIG | jq -r '.database.sslMode // empty'`"
-  export DB_CAFILE="/etc/pgbouncer/rdsca.cert"
-  export PG_BOUNCER_LISTEN_PORT="`cat $ACG_CONFIG | jq -r '.webPort'`"
+elif [[ ! -f "${env_file}" ]]; then
+  python3 /json_to_env.py --prefix "CLOWDER_" "${ACG_CONFIG}" > "${env_file}"
+fi
 
-  db_cert="`cat $ACG_CONFIG | jq -r '.database.rdsCa // empty'`"
+if [[ -f "${env_file}" && -n "${ACG_CONFIG}" ]]; then
+  source "${env_file}"
+
+  export DB_NAME="${CLOWDER_DATABASE_NAME}"
+  export DB_HOST="${CLOWDER_DATABASE_HOSTNAME}"
+  export DB_PORT="${CLOWDER_DATABASE_PORT}"
+  export DB_USER="${CLOWDER_DATABASE_USERNAME}"
+  export DB_PASSWORD="${CLOWDER_DATABASE_PASSWORD}"
+  export DB_SSLMODE="${CLOWDER_DATABASE_SSLMODE}"
+  export DB_CAFILE="/etc/pgbouncer/rdsca.cert"
+  export PG_BOUNCER_LISTEN_PORT="${CLOWDER_WEBPORT}"
+
+  db_cert="${CLOWDER_DATABASE_RDSCA}"
   >${DB_CAFILE}
   if [[ -n "${db_cert}" ]]; then
     echo "${db_cert}" > ${DB_CAFILE}
